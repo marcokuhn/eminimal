017ebd68b229d00c30bc61c4e255119643b028a8
Matthias Georgi <matthias@soundcloud.com>
2014-02-17 14:48:10 +0100

    minor tweaks

--- personal/setup-company.el
+++ personal/setup-company.el
@@ -2,10 +2,10 @@
 
 (require 'company)
 
-(setq company-idle-delay 0)
 (setq company-tooltip-limit 30)
-(setq company-minimum-prefix-length 1)
 (setq company-require-match nil)
+(setq company-idle-delay 0.1)
+(setq company-minimum-prefix-length 1)
 
 (define-key company-mode-map (kbd "TAB") 'company-complete)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
