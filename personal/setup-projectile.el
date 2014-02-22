656e7697c1ed7b9ac3d3663af3184b99cf5e4ec3
Matthias Georgi <matthias@soundcloud.com>
2014-02-22 14:22:17 +0100

    projectile switch to buffer

--- personal/setup-projectile.el
+++ personal/setup-projectile.el
@@ -10,5 +10,6 @@
 (setq projectile-completion-system 'grizzl)
 
 (global-set-key (kbd "s-f") 'projectile-find-file)
+(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
 (global-set-key (kbd "s-d") 'projectile-find-dir)
 (global-set-key (kbd "s-p") 'projectile-switch-project)
